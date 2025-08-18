0538346b2ffd8cca3edd68989858b68acb4758fb
Shobhit Aggarwal <shobhita@andrew.cmu.edu>
2025-08-18 17:10:55 -0400

    remove version and home page title

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@
 project = 'MFI Learning Lab'
 copyright = 'CMU'
 author = 'Manufacturing Futures Institute'
-release = '1.0'
+# release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
